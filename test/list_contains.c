3ad10fe99ba370551a3b5895e870f86458ce5578
Adrien Brignon <adrien@brignon.dev>
2020-09-21 21:02:58 +0200

    Now destroying list once test is done

--- test/list_contains.c
+++ test/list_contains.c
@@ -11,12 +11,14 @@
 
 Test(list_contains, should_contain)
 {
-    char *bonjour = "bonjour";
     list_t *list = list_new();
+    const char *bonjour = "bonjour";
 
     cr_assert_not_null(list);
     cr_assert_eq(list_push(list, "hey"), 1);
     cr_assert_eq(list_push(list, "hola"), 2);
     cr_assert_eq(list_push(list, bonjour), 3);
     cr_assert_eq(list_contains(list, bonjour), 1);
+    
+    list_destroy(list);
 }
